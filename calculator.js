ca2d45c717ba49cbe3835e35b15c06752b74aafe
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-08 20:13:15 +0300

    Delete calculator.js

--- calculator.js
+++
@@ -1,7 +0,0 @@
-export const getSquaredArray = num =>
-    num.map(num => num * num)
-
-export const getOddNumbers = num =>
-    num.filter(num => (num % 2 === 1))
-
-export default (a, b) => a + b;
